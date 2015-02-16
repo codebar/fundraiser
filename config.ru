9d6cd09687933841d50a5fa8c1da1e1d289a0b88
Despo Pentara <despo@extractmethod.com>
2015-02-16 20:35:04 +0000

    Codebar donation page!

---
+++ config.ru
@@ -0,0 +1,3 @@
+require './lib/fundraiser'
+
+run Fundraiser::App
---
+++ lib/fundraiser.rb
@@ -0,0 +1,39 @@
+require 'sinatra/base'
+require 'stripe'
+
+module Fundraiser
+  class App < Sinatra::Base
+    set :publishable_key, ENV['PUBLISHABLE_KEY']
+    set :secret_key, ENV['SECRET_KEY']
+    set :views, "#{Dir.pwd}/views"
+
+    Stripe.api_key = settings.secret_key
+
+    get '/' do
+      erb :index
+    end
+
+    post '/charge' do
+      @amount = 65000
+
+      customer = Stripe::Customer.create(
+        :email => params[:stripeEmail],
+        :card  => params[:stripeToken]
+      )
+
+      charge = Stripe::Charge.create(
+        :amount      => @amount,
+        :description => 'Codebar Donation',
+        :currency    => 'gbp',
+        :customer    => customer.id
+      )
+
+      erb :charge
+
+    end
+
+    error Stripe::CardError do
+      env['sinatra.error'].message
+    end
+  end
+end
